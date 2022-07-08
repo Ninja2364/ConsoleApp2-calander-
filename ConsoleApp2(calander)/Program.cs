6cf67cfa9931b1ff98fcb7758d0fc5bc032834c9
Andrew Wang <arwsushi@gmail.com>
2022-07-08 16:25:52 -0400

    Add project files.

---
+++ ConsoleApp2(calander)/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2_calander_
+{
+    internal class Program
+    {
+        class ftime
+        {
+            public static void Main(string[] args)
+            {
+                string date = DateTime.Today.ToString();
+                Console.WriteLine("Today is " + date);
+            }
+            
+        }
+    }
+}

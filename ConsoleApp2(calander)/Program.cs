d77aa140537bd191da2ce51c7ad2e4d3aee127b9
Andrew Wang <arwsushi@gmail.com>
2022-07-11 14:33:49 -0400

    start

--- ConsoleApp2(calander)/Program.cs
+++ ConsoleApp2(calander)/Program.cs
@@ -12,9 +12,27 @@ namespace ConsoleApp2_calander_
         {
             public static void Main(string[] args)
             {
-                string date = DateTime.Today.ToString();
-                Console.WriteLine("Today is " + date);
+                string dt = DateTime.Today.ToString("12-dd-yyyy");
+                Console.WriteLine("Today is " + dt);
+                Console.Write(dt[0]); Console.WriteLine(dt[1]);
+
+                //gets month into a int
+                string month = DateTime.Today.ToString("MM");
+                Convert.ToInt32(month);
+                //get month into an name
+                string monthname = DateTime.Today.ToString("MMMM");
+
+                //Keep as MM for number value
+                Console.WriteLine(month);
+                //get *day*
+                string day = DateTime.Today.ToString("dd");
+                Console.WriteLine(day);
+
+
+                Console.WriteLine("Today is the " + day + " of " + monthname);
+
             }
+
             
         }
     }

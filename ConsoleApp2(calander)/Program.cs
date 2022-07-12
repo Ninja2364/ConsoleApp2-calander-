cfefd7e864c2b9a39d6fe4b68065852aaa6a6080
Andrew Wang <arwsushi@gmail.com>
2022-07-11 21:47:45 -0400

    a

--- ConsoleApp2(calander)/Program.cs
+++ ConsoleApp2(calander)/Program.cs
@@ -1,39 +1,119 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ConsoleApp2_calander_
 {
     internal class Program
     {
-        class ftime
+
+        public class ftime
         {
-            public static void Main(string[] args)
+             static void Main(string[] args)
             {
-                string dt = DateTime.Today.ToString("12-dd-yyyy");
+                 string dt = DateTime.Today.ToString("12-dd-yyyy");
                 Console.WriteLine("Today is " + dt);
                 Console.Write(dt[0]); Console.WriteLine(dt[1]);
 
                 //gets month into a int
-                string month = DateTime.Today.ToString("MM");
-                Convert.ToInt32(month);
-                //get month into an name
-                string monthname = DateTime.Today.ToString("MMMM");
+                
+                
+                
+                //get month into an name 
+                 string monthname = DateTime.Today.ToString("MMMM");
 
-                //Keep as MM for number value
-                Console.WriteLine(month);
+                
                 //get *day*
-                string day = DateTime.Today.ToString("dd");
+                 string day = DateTime.Today.ToString("dd");
                 Console.WriteLine(day);
 
 
                 Console.WriteLine("Today is the " + day + " of " + monthname);
 
-            }
+               
+                miscctime();
+                
+              
 
-            
+
+
+
+
+
+
+
+                //Find leap and parts of... year, days of that certain month (relate with leaps)
+
+                 void miscctime()
+                {
+                    //Find how many days are in the month *ADD LEAPS SOME OTHER DAY*
+                    
+
+
+                    string month = DateTime.Today.ToString("MM");
+                    //monthx is now int version
+                    int monthx = Convert.ToInt32(month);
+                       Console.WriteLine(month);
+
+                    //for checks change monthhx remove // 
+                    //monthx = 3;
+                    int daysinmonth;
+                    daysinmonth = Convert.ToInt32(day);
+                    Console.WriteLine(day + "check");
+                   /* if (monthx == 7)
+                    {
+                        daysinmonth = 31;
+                    }
+                   */
+                    switch (monthx)
+                    {
+                        case 1:
+                            daysinmonth = 31;
+                            break;
+                        case 3:
+                            daysinmonth = 31;
+                            break;
+                        case 5:
+                            daysinmonth = 31;
+                            break;
+                        case 7:
+                            daysinmonth = 31;
+                            Console.WriteLine("heard");
+                            break;
+                        case 9:
+                            daysinmonth = 31;
+                            break;
+                        case 11:
+                            daysinmonth = 31;
+                            break;
+                        //check for the ones that are 30 days
+                        case 4:
+                            daysinmonth = 30;
+                            break;
+                        case 6:
+                            daysinmonth = 30;
+                            break;
+                        case 8:
+                            daysinmonth = 30;
+                            break;
+                        case 10:
+                            daysinmonth = 30;
+                            break;
+                        case 12:
+                            daysinmonth = 30;
+                            break;
+                    }
+                            Console.WriteLine("there are " + daysinmonth);
+                    
+
+
+
+                }
+
+
+
+
+
+
+            }
         }
     }
 }
